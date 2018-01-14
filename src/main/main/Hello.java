efad42d3b7c489b02087c9c81d3896fee43466df
Paweł Leśniak <pawemix@gmail.com>
2018-01-14 19:12:42 +0100

    changes in various procedures.

--- src/main/main/Hello.java
+++ src/main/main/Hello.java
@@ -1,9 +1,13 @@
 package main;
 
+import database.DatabaseConnector;
+
 public class Hello {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello database adventure");
+		DatabaseConnector dbc = new DatabaseConnector();
+		dbc.rebuildDatabase();
 	}
 
 }
